bddd4ce0314d5acf66eb224d8be05a3119be8158
Piotr Trendak <piotr.trendak@decsoft.com.pl>
2018-07-16 16:32:31 +0200

    corect login

--- modules/login.php
+++ modules/login.php
@@ -1,32 +1,50 @@
 <h1>Logowanie</h1>
 <?php 
 	session_start();
+	unset($_SESSION['error']);
 if (isset($_POST['login']) && isset($_POST['password'])) {
 		$login = $_POST['login'];
 	$password = $_POST['password'];
 
 	//anty wstrzykiwanie sql
 	$login = htmlentities($login, ENT_QUOTES, "UTF-8");
+
 	$password = htmlentities($password, ENT_QUOTES, "UTF-8");
 	$result = $pdo->prepare('SELECT * FROM users WHERE login = :login AND password = :password');
 	$result->bindParam('login', $login);
 	$result->bindParam('password', $password);
 	$result->execute();
-	$login = $result->fetchAll();
-	echo $login['login'];
-	if ($result) {
-		var_dump($result);
+	$login = $result->fetch();
+	if ($login) {
+		$_SESSION['loged'] = true;
+		$_SESSION['login'] = $login['login'];
+		
+		unset($_SESSION['error']);
+		$result->closeCursor();
+	}
+	else{
+		$_SESSION['error'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
+		
+
 	}
 
 
 }
-
+if (isset($_GET['logout'])==1) 
+{
+	$_SESSION['logout'] = false;
+	session_destroy();
+}
 
 
  ?>
 
 
+
 <form method="POST">
+	<?php
+	if(isset($_SESSION['error']))	echo $_SESSION['error'];
+?>
 	<div class="">
 		<label for="login">Login</label>
 		<input type="text" name="login" class="">
