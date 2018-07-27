289301d8551e330aaa11a5a776dec045a2953352
Piotr Trendak <piotr.trendak@decsoft.com.pl>
2018-07-27 16:30:32 +0200

    users panel

---
+++ layouts/user.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr.trendak
+ * Date: 2018-07-27
+ * Time: 11:39
+ */
---
+++ modules/delete_page.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr.trendak
+ * Date: 2018-07-24
+ * Time: 13:35
+ */
